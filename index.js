fb490ae82cdc26080619046fd9afa099df5d861a
Sara Manaf <sara.0297859@gmail.com>
2022-10-25 15:54:03 +0300

    DONEE

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+function multiply(num1,num2)
+{
+return num1*num2;
+}
+multiply(8,6)
+function random(num3)
+{
+    if (num3>0)
+    return Math.random;
+}
+random(4);
+let counter=12;
+counter %= 6;
+ Number.MAX_VALUE(12,4,5);
